896151c0259ee896baf9827f96526afd86e02b5b
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-10-20 10:42:32 +0200

    0.0.10

--- life-diary.js
+++ life-diary.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
+/**
+ * ISC License
+ *
+ * Copyright (c) 2020, Andrea Giammarchi, @WebReflection
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
 const {extname, join, resolve} = require('path');
 const {rmdir, unlink, mkdir, readFile, readdir, writeFile} = require('fs');
 
@@ -15,6 +34,7 @@ const IPv4 = include('IPv4');
 
 // SERVER
 const auth = require('basic-auth');
+const compress = require('compression');
 const mime = require('mime-types');
 const express = require('express');
 const fileUpload = require('express-fileupload');
@@ -47,6 +67,7 @@ app.use(fileUpload({
   tempFileDir: TMP
 }));
 
+app.use(compress());
 app.use(express.static(PUBLIC));
 
 app.use(bodyParser.json());
--- public/js/exif.js
+++ public/js/exif.js
@@ -1,4 +1,24 @@
+/**
+ * ISC License
+ *
+ * Copyright (c) 2020, Andrea Giammarchi, @WebReflection
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
 const DMS = /^(-?\d+(?:\.\d+)?)[°:d]?\s?(?:(\d+(?:\.\d+)?)['′ʹ:]?\s?(?:(\d+(?:\.\d+)?)["″ʺ]?)?)?\s?([NSEW])?/i;
+const {abs} = Math;
 
 export default {
   convert(dms) {
@@ -7,15 +27,15 @@ export default {
       const d = parseFloat(deg);
       const m = (parseFloat(min) / 60) || 0;
       const s = (parseFloat(sec) / 3600) || 0;
-      return /[SW]/i.test(hem) ? (-Math.abs(d) - m - s) : (d + m + s);
+      return /[SW]/i.test(hem) ? (-abs(d) - m - s) : (d + m + s);
     }
     return NaN;
   },
   coords({GPSMapDatum, GPSLatitude, GPSLatitudeRef, GPSLongitude, GPSLongitudeRef}) {
     const coords = [];
     if (/WGS.*84/i.test(GPSMapDatum) && GPSLatitude && GPSLatitudeRef && GPSLongitude && GPSLongitudeRef) {
-      const lat = this.convert(`${GPSLatitude.replace(/\s+deg/i, '°')} ${GPSLatitudeRef[0]}`);
-      const long = this.convert(`${GPSLongitude.replace(/\s+deg/i, '°')} ${GPSLongitudeRef[0]}`);
+      const lat = this.convert(`${GPSLatitude.replace(/\s*deg/i, '°')} ${GPSLatitudeRef[0]}`);
+      const long = this.convert(`${GPSLongitude.replace(/\s*deg/i, '°')} ${GPSLongitudeRef[0]}`);
       if (!isNaN(lat) && !isNaN(long))
         coords.push(lat, long);
     }
